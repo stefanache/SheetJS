43a249e03297920b59fd56268b8645c6cc60cc0b
Stefanache Ion <ion.stefanache@yahoo.com>
2023-12-10 13:34:39 +0200

    Update test1.bat

--- test1.bat
+++ test1.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-echo check to see if hve php installed on your computer...
+echo Firstly, check to see if hve php installed on your computer...
 where php
 php -V
 
@@ -11,11 +11,12 @@ echo ...which get the index-page from PhP's webserver...
 
 echo ...so, now starting the stand-alone PhP's webserver...
 php -S localhost:9090 |more
-echo press ENTER to exit from /stop the locally PhP's webserver 
+echo press ENTER to exit from /stop the locally PhP's webserver!
 pause
 
 echo Note: you can use/call the index-page directly from ghithub ...
 start Chrome https://stefanache.github.io/SheetJS/index.html
+echo but the file upload from locally computer's device/disk!
 pause
 
 cls
